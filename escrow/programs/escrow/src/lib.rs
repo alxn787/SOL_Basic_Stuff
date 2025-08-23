7a59a4c0eecfa84def62aac1762c12e531c8b83c
alxn787 <alenabraham787@gmail.com>
2025-08-23 11:37:41 +0530

    escrow init

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -6,11 +6,20 @@ declare_id!("4rzg2P9UcKgwbuCbSYyw5XGV3Z5Uxxit85tuTK1tHYkU");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_escrow(ctx: Context<InitializeEscrow>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeEscrow {
+
+}
+
+#[account]
+pub struct Escrow {
+    pub authority : Pubkey,
+    pub recipient : Pubkey,
+    pub vault_bump : u8,
+}
