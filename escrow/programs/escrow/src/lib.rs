2aacda9a2c2648761a507371bb3bceb40f9652a3
alxn787 <alenabraham787@gmail.com>
2025-08-24 21:10:10 +0530

    init escrow

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -5,16 +5,28 @@ declare_id!("4rzg2P9UcKgwbuCbSYyw5XGV3Z5Uxxit85tuTK1tHYkU");
 
 #[program]
 pub mod escrow {
+    use anchor_spl::token::Transfer;
+
     use super::*;
 
-    pub fn initialize_escrow(ctx: Context<InitializeEscrow>) -> Result<()> {
+    pub fn initialize_escrow(ctx: Context<InitializeEscrow>, amount: u64) -> Result<()> {
         let escrow = &mut ctx.accounts.escrow;
 
         escrow.authority = ctx.accounts.authority.key();
         escrow.recipient = ctx.accounts.recipient.key();
         escrow.mint = ctx.accounts.mint.key();
-        escrow.amount = 0;
+        escrow.amount = amount;
         escrow.vault_authority_bump = ctx.bumps.vault_authority;
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.associated_token_program.to_account_info(),
+            Transfer{
+                from : ctx.accounts.authority_token_account.to_account_info(),
+                to : ctx.accounts.escrow_token_account.to_account_info(),
+                authority : ctx.accounts.authority.to_account_info(),
+            },
+        );
+        anchor_spl::token::transfer(cpi_ctx, amount)?;
 
         Ok(())
     }
@@ -49,6 +61,9 @@ pub struct InitializeEscrow<'info> {
     #[account(mut)]
     pub authority : Signer<'info>,
 
+
+    pub authority_token_account : Account<'info, TokenAccount>,
+
     #[account(mut)]
     pub recipient : AccountInfo<'info>,
 
@@ -56,6 +71,39 @@ pub struct InitializeEscrow<'info> {
     pub token_program : Program<'info, Token>,
     pub associated_token_program : Program<'info, AssociatedToken>,
     pub rent : Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct ReleaseEscrow<'info> {
+    #[account(
+        mut,
+        has_one = recipient,
+        
+    )]
+    pub escrow : Account<'info, Escrow>,
+
+    #[account(mut,
+        seeds = [b"vault_authority".as_ref(), escrow.key().as_ref()],
+        bump,
+    )]
+    /// CHECK: This is the vault authority account
+    pub vault_authority : UncheckedAccount<'info>,
+
+    pub mint : Account<'info, Mint>,
+
+    #[account(
+        constraint = escrow_token_account.mint == mint.key(),
+        constraint = escrow_token_account.owner == vault_authority.key(),
+    )]
+    pub escrow_token_account : Account<'info, TokenAccount>,
+
+    #[account(
+        constraint = recipient_token_account.mint == mint.key(),
+        constraint = recipient_token_account.owner == recipient.key(),
+    )]
+    pub recipient_token_account : Account<'info, TokenAccount>,
+
+    pub recipient : Signer<'info>,
 }
 
 #[account]
